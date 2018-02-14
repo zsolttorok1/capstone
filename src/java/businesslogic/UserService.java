fdac5d42473ba8f936db56748e21d3f1c1c379d6
742227 <742227@ELF6IT215805.ACDM.DS.SAIT.CA>
2018-02-14 16:22:13 -0700

    Capstone: Added user servlet shell and user.java as well as user service

---
+++ src/java/businesslogic/UserService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businesslogic;
+
+/**
+ *
+ * @author 742227
+ */
+public class UserService {
+    
+}
