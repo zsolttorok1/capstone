ddab3a1949f26a2fa081f69391c960e039a63e16
679918 <679918@ELF6IT215951.ACDM.DS.SAIT.CA>
2018-02-16 08:50:12 -0700

    userBroker and userService edited

--- src/java/businesslogic/UserService.java
+++ src/java/businesslogic/UserService.java
@@ -1,18 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package businesslogic;
 
+import dataaccess.UserBroker;
+import domainmodel.User;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
-//    public String addUser() {
-//       
-//    }
-//    
-//    public User viewUser() {
-//
-//    }
+    public String addUser(String userName, int address, ArrayList<Integer> phone, String password, String firstName, String lastName,String role, String email, int hourlyRate, int hours) {
+    //check for nulls
+    if(userName == null || address == 0 || phone.isEmpty() || password == null || firstName == null || lastName == null ||
+             role == null || email == null || hourlyRate == 0 || hours == 0 ||
+            userName.isEmpty() || password.isEmpty() || firstName.isEmpty() || lastName.isEmpty() ||
+             role.isEmpty() || email.isEmpty()){
+        return "error";
+    }
+    
+    //check for values being valid
+    
+    User user = new User(userName, address, phone, password, firstName, lastName, role, email, hourlyRate, hours);
+    
+    UserBroker userBroker = new UserBroker();
+    return userBroker.insert(user);
+    }
+    
+    public User viewUser(String userName) {
+       UserBroker userBroker = new UserBroker();
+       
+       User user = userBroker.getByName(userName);
+       
+       return user;
+    }
 //        
 //    public User<User> searchUser() {
 //
