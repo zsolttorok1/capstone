b175f5f2c513300026bc37cb33af1c28ba1d9a81
679918 <679918@ELF6IT215951.ACDM.DS.SAIT.CA>
2018-03-01 13:05:24 -0700

    added the report, reportbroker and reportservice

---
+++ src/java/businesslogic/ReportService.java
@@ -0,0 +1,11 @@
+package businesslogic;
+
+import dataaccess.ReportBroker;
+import domainmodel.Report;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ReportService {
+    
+}
