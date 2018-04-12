85c951bb09ee4f12bc6c47c81273389b617c9ec4
Steven <Steven@192.168.1.66>
2018-04-12 00:42:58 -0600

    consistency updates + Quote Service javadoc

--- src/java/businesslogic/QuoteService.java
+++ src/java/businesslogic/QuoteService.java
@@ -9,11 +9,21 @@ import domainmodel.Quote;
 import java.util.List;
 
 /**
- *
- * @author 742227
+ * Quote Service handles all the operations regarding the Quote Object, including adding, viewing, deleting
+ * and searching.
+ * 
  */
 public class QuoteService {
-     public String addQuote(String name, String description, String email) {
+
+    /**
+     * Inserts new Quote Object into database using the QuoteBroker.
+     *
+     * @param name name of quote as a String
+     * @param description description(information) of quote as a String
+     * @param email email address of potential customer writing the quote as a String
+     * @return status of quote insertion
+     */
+    public String addQuote(String name, String description, String email) {
         //check for nulls
         if (name == null || description == null || email == null || name.isEmpty()
                 || description.isEmpty() || email.isEmpty()) {
@@ -27,6 +37,12 @@ public class QuoteService {
         return quoteBroker.insert(quote);
     }
 
+    /**
+     * Loads all data of a Quote Object matching unique quote ID to view
+     *
+     * @param quoteId unique quote ID as a String
+     * @return quote Object
+     */
     public Quote viewQuote(String quoteId) {
         QuoteBroker quoteBroker = new QuoteBroker();
         Quote quote = null;
@@ -41,6 +57,12 @@ public class QuoteService {
         return quote;
     }
 
+    /**
+     * Deletes an existing Quote object from database based on its unique quote ID.
+     *
+     * @param quoteId unique quote ID as a String
+     * @return status of deletion
+     */
     public String delete(String quoteId) {
         QuoteBroker quoteBroker = new QuoteBroker();
         String status = "";
@@ -58,6 +80,12 @@ public class QuoteService {
         return status;
     }
     
+    /**
+     * Searches through all existing quote objects for matching keyword using quoteBroker.
+     *
+     * @param keyword search term entered
+     * @return  list of quote Objects which match keyword
+     */
     public List<Quote> searchQuote(String keyword) {
         QuoteBroker quoteBroker = new QuoteBroker();
         return quoteBroker.getAll();
